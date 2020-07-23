a833b740cdf1a42c35fffa54450c5b695adae5e5
Victor <victorribera92@gmail.com>
2020-07-23 13:52:48 +0200

    rooms

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,8 @@ import Hero from "../components/Hero";
 import Banner from "../components/Banner";
 import {Link} from "react-router-dom";
 import Services from "../components/Services";
-import FeaturedRooms from "../components/FeaturedRooms"
+import FeaturedRooms from "../components/FeaturedRooms";
+
 
 export default function Home() {
     return (
--- src/pages/Rooms.js
+++ src/pages/Rooms.js
@@ -2,15 +2,24 @@ import React from 'react';
 import Hero from "../components/Hero";
 import Banner from "../components/Banner";
 import {Link} from "react-router-dom";
+import RoomContainer from "../components/RoomContainer";
+
+
+
 
 export default function Rooms() {
     return (
-        <div>
-            <Hero hero="roomsHero">
-                <Banner title="Our rooms">
-                    <Link to="/" className="btn-primary">Return home</Link>
-                </Banner>
-            </Hero>
-        </div>
+        <>
+            <div>
+                <Hero hero="roomsHero">
+                    <Banner title="Our rooms">
+                        <Link to="/" className="btn-primary">Return home</Link>
+                    </Banner>
+                </Hero>
+            </div>
+            <RoomContainer>
+
+            </RoomContainer>
+        </>
     )
 }
